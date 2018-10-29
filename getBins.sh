6a8421be869e3c666b5c884dcb8924828e85c549
Pierre LE CORRE <pierre.lecorre.free@gmail.com>
2018-10-29 09:32:22 +0100

    Update getBins.sh against shellcheck

--- getBins.sh
+++ getBins.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if !( [[ $1 =~ [0-9]\.[0-9]\.[0-9] ]] )
+if ! [[ $1 =~ [0-9]\.[0-9]\.[0-9] ]]
 then
-  echo "usage: $0 (release number) (Os)"
+  echo "usage: $0 (release number)"
   echo "release number format: [0-9].[0-9].[0-9]"
   exit 1
 fi
@@ -12,7 +12,7 @@ fi
 RELEASE_NUMBER=$1
 PLATFORMS=( "linux" "darwin" "windows" )
 
-rm -r src/assets/bin
+rm -rf src/assets/bin
 
 for PLATFORM in "${PLATFORMS[@]}"
 do
@@ -21,15 +21,15 @@ do
   if [ "$PLATFORM" != "windows" ]
   then
     curl ${CURL_PARAMETERS}
-    tar -xzf woleet-cli_${PLATFORM}_x86_64.tar.gz
-    rm woleet-cli_${PLATFORM}_x86_64.tar.gz
-    mkdir -p src/assets/bin/${PLATFORM}
-    mv woleet-cli src/assets/bin/${PLATFORM}/woleet-cli
+    tar -xzf "woleet-cli_${PLATFORM}_x86_64.tar.gz"
+    rm "woleet-cli_${PLATFORM}_x86_64.tar.gz"
+    mkdir -p "src/assets/bin/${PLATFORM}"
+    mv woleet-cli "src/assets/bin/${PLATFORM}/woleet-cli"
   else
     curl ${CURL_PARAMETERS//'.tar.gz'/'.zip'}
-    unzip -q woleet-cli_${PLATFORM}_x86_64.zip
-    rm woleet-cli_${PLATFORM}_x86_64.zip
-    mkdir -p src/assets/bin/${PLATFORM}
-    mv woleet-cli.exe src/assets/bin/${PLATFORM}/woleet-cli
+    unzip -q "woleet-cli_${PLATFORM}_x86_64.zip"
+    rm "woleet-cli_${PLATFORM}_x86_64.zip"
+    mkdir -p "src/assets/bin/${PLATFORM}"
+    mv woleet-cli.exe "src/assets/bin/${PLATFORM}/woleet-cli"
   fi
 done
