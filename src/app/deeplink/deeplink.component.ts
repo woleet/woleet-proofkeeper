5fa9979e6280afcefaf75d36351d597589fc7734
Pierre LE CORRE <pierre.lecorre.free@gmail.com>
2020-02-07 16:22:28 +0100

    WOLEET-1509 Working for wids

--- src/app/deeplink/deeplink.component.ts
+++ src/app/deeplink/deeplink.component.ts
@@ -1,9 +1,15 @@
 import { Component, Inject } from '@angular/core';
-import {MAT_DIALOG_DATA  } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 import { MatDialogRef } from '@angular/material/dialog';
 import { WoleetCliParametersService } from '../services/woleetcliParameters.service';
 import { IdentityService } from '../services/Identity.service';
+import { noDuplicateIdentityNameValidatorFactoryOnAdd } from '../misc/validators';
+import { checkwIDConnectionGetAvailableKeys } from '../misc/settingsChecker';
+import { PubKeyAddressGroup } from '../misc/identitiesFromServer';
 import * as log from 'loglevel';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-wizard',
@@ -16,17 +22,29 @@ export class DeeplinkComponent {
   public isWids = false;
   public token: string;
   public url: string;
+  public pubKeyAddressGroup: PubKeyAddressGroup[];
+  public widsFormGroup: FormGroup;
 
   constructor(private dialogRef: MatDialogRef<DeeplinkComponent>,
+    private formBuilder: FormBuilder,
     private cli: WoleetCliParametersService,
     public identityService: IdentityService,
+    private snackBar: MatSnackBar,
+    private http: HttpClient,
     @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.pubKeyAddressGroup = [];
     const deeplinkingUrl = new URL(data.url);
     this.checkParameters(deeplinkingUrl);
     if (this.isApi || this.isWids) {
       this.token = deeplinkingUrl.searchParams.get('token');
       if (deeplinkingUrl.searchParams.has('url')) {
         this.url = deeplinkingUrl.searchParams.get('url');
+      }
+      if (this.isWids) {
+        this.widsFormGroup = formBuilder.group({
+          name: ['', [Validators.required, noDuplicateIdentityNameValidatorFactoryOnAdd(this)]],
+          pubKey: ['', [Validators.required]]
+        });
       }
     }
   }
@@ -42,11 +60,22 @@ export class DeeplinkComponent {
       this.isApi = true;
       return;
     } else if (deeplinkingUrl.pathname === '//wids') {
-      if (!deeplinkingUrl.searchParams.has('token') && deeplinkingUrl.searchParams.has('url')) {
+      if (deeplinkingUrl.searchParams.has('token') && deeplinkingUrl.searchParams.has('url')) {
+        this.isWids = true;
         return;
       }
-      this.isWids = true;
       return;
+    }
+  }
+
+  async onClickCheckwIDConnectionGetAvailableKeys() {
+    await checkwIDConnectionGetAvailableKeys(this.http,
+    this.url,
+    this.token,
+    this.pubKeyAddressGroup,
+    this.snackBar);
+    if (this.pubKeyAddressGroup.length === 0) {
+      this.isWids = false;
     }
   }
 
@@ -59,7 +88,15 @@ export class DeeplinkComponent {
       }
     }
     if (this.isWids) {
-
+      this.identityService.addIdentity(
+        this.widsFormGroup.get('name').value,
+        this.url,
+        this.token,
+        this.widsFormGroup.get('pubKey').value);
+      this.widsFormGroup.reset();
+      while (this.pubKeyAddressGroup.length) {
+        this.pubKeyAddressGroup.pop();
+      }
     }
     this.dialogRef.close();
   }
